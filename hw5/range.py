cf19a6627297f862e71e75c08c962211f83db524
Darklurker <foxover.rit@gmail.com>
2023-01-07 05:35:17 +0200

    TODO: the third case

---
+++ hw5/range.py
@@ -0,0 +1,17 @@
+"""
+2) користувач вводить числа a, b  вивиести всі цілі числа між ними
+"""
+
+a = int(input(f"Введіть a: "))
+b = int(input(f"Введіть b: "))
+line = ''
+if a + 1 < b:
+    for i in range(a + 1, b, 1):
+        line += f" {i}"
+    print(line)
+elif a > b + 1:
+    for i in range(b + 1, a, 1):
+        line += f" {i}"
+    print(line)
+else:
+    print("Між a та b не має цілих чисел")
