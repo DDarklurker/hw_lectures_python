ab250177123263e023b8ddabdf8c36e08f1f8ce0
Darklurker <foxover.rit@gmail.com>
2023-01-07 07:41:05 +0200

    TODO: fix

--- hw5/dividible.py
+++ hw5/dividible.py
@@ -6,21 +6,30 @@
 a = int(input(f"Введіть a між 1 та 200: "))
 b = int(input(f"Введіть b між 1 та 200: "))
 line = ''
+count = 0
 if 0 < a < 200 and 0 < b <= 200:
     if a + 1 < b:
         for j in range(2, 7, 1):
             for i in range(a + 1, b, 1):
+                if count == 10:
+                    break
                 if i % j == 0:
                     line += f" {i},"
+                    count += 1
             print(f"Dividible by {j}:{line}")
             line = ''
+            count = 0
     elif a > b + 1:
         for j in range(2, 7, 1):
             for i in range(b + 1, a, 1):
+                if count == 10:
+                    break
                 if i % j == 0:
                     line += f" {i},"
+                    count += 1
             print(f"Dividible by {j}:{line}")
             line = ''
+            count = 0
     else:
         print("Між a та b не має цілих чисел")
 else:
