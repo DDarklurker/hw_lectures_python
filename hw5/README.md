f784ffbfb6e150e6e720fb23ce83509aea0f6fea
Darklurker <foxover.rit@gmail.com>
2023-01-07 07:48:52 +0200

    TODO: question

--- hw5/README.md
+++ hw5/README.md
@@ -1,2 +1,6 @@
 Homework for 5 lecture 
 
+
+Question:
+Як перевіряти float для подальшого вписання?
+
---
+++ hw5/question.py
@@ -0,0 +1,3 @@
+your_pwd = input(f"Введіть пароль для перевірки: ")
+if your_pwd.isnumeric():
+    print(your_pwd)
