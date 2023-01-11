d5b1c00915a90c3f9985780515c808668a010b8c
Darklurker <foxover.rit@gmail.com>
2023-01-12 00:11:21 +0200

    TODO: 6) with *

--- hw6/in_one_numbet.py
+++ hw6/in_one_numbet.py
@@ -4,7 +4,27 @@
 """
 
 def join_ints(my_list):
-    pass
+    result = ''
+    for i in range(len(my_list)):
+        if str(my_list[i]).isdigit():
+            result += str(my_list[i])
+    result = int(result)
+    return result
 
+
+def join_ints(my_list):
+    result = 0
+    count = 0
+
+    for i in range(len(my_list) - 1, -1, -1):
+        if (type(my_list[i])) is int:
+            result += my_list[i] * (10 ** count)
+            count += 1
+
+    return result
+
+
+print(join_ints([1, 2, 3]))
+print(join_ints([1, "foo", 2.5, 1, 1, 4, "abr", 3]))
 assert join_ints([1, 2, 3]) == 123
 assert join_ints([1, "foo", 2.5, 1, 1, 4, "abr", 3]) == 11143
