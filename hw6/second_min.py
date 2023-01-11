3ef7f2cd2b6cc914849327405c11abe5f5dfd596
Darklurker <foxover.rit@gmail.com>
2023-01-11 18:37:29 +0200

    TODO: 2)

--- hw6/second_min.py
+++ hw6/second_min.py
@@ -4,8 +4,18 @@
 2) без використання методів списків
 """
 
-def second_smallest(array):
-    pass
 
+def second_smallest(array):
+    array.sort()
+    return array[1]
+test1 = [1, 2, 2, 3]
+test2 = [-1, 10, -2, 2]
+
+
+
+
+
+print(second_smallest([1, 2, 2, 3]))
+print(second_smallest([-1, 10, -2, 2]))
 assert second_smallest([1, 2, 2, 3]) == 2
 assert second_smallest([-1, 10, -2, 2]) == -1
