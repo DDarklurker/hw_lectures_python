8f598437b413ed1f2c66f47264949f6269d00f68
Darklurker <foxover.rit@gmail.com>
2023-01-20 23:52:43 +0200

    TODO: need to update with another divide

---
+++ hw8/rational_num.py
@@ -0,0 +1,44 @@
+"""
+складати і скорочуавти раціональні дроби .
+"""
+import math
+
+
+def divide(a, b):
+    c = []
+    if len(a) != 2 or len(b) != 2:
+        print('Вказані не вірні значення.')
+        return None
+    if a[1] == 0 or b[1] == 0:
+        return None
+    if a[1] != b[1]:
+        c.append(a[0] * b[1] + a[1] * b[0])
+        c.append(a[1] * b[1])
+    else:
+        c.append(b[0] + a[0])
+        c.append(a[1])
+
+    def rational(num):
+        if num[0] == -2 or num[0] == 2:
+            if num[1] % 2 == 0:
+                num[1] = num[1] // int(math.fabs(num[0]))
+                num[0] = num[0] // int(math.fabs(num[0]))
+            return num
+        else:
+            index = int(math.ceil(num[0] // 2))
+        for i in range(2, index + 1, 1):
+            if num[0] == 1:
+                return num
+            if num[0] % i == 0:
+                num[1] = num[1] // i
+                num[0] = num[0] // i
+                num = rational(num)
+        return num
+
+    c = tuple(rational(c))
+    return c
+
+
+"1/8 + 3/16"
+assert divide((1, 8), (3, 8)) == (1, 2)  # 1\8 + 3\8 = 4\8 = 1\2
+assert divide((1, 0), (3, 8)) == None  # 1\0 + 3\8 - not valid
