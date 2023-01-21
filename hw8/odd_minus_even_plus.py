5c6c4a37558422f6c02b654a8292e0b500931d46
Darklurker <foxover.rit@gmail.com>
2023-01-21 13:09:10 +0200

    TODO: odd_minus_even_plus

--- hw8/odd_minus_even_plus.py
+++ hw8/odd_minus_even_plus.py
@@ -6,9 +6,10 @@
 sum = 0
 while True:
     number = (input("Введіть чьотне або нечьотне число: "))
-    if number.isdigit():
+    if number.isdigit() or number.replace('-', '', 1).isdigit():
         number = int(number)
         sum = sum + number if number % 2 == 0 else sum - number
+
     else:
         if number == 'end':
             break
