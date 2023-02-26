b7b4d560cab8df6394739db3759def0e58e040e3
Darklurker <foxover.rit@gmail.com>
2023-02-26 13:48:18 +0200

    hw16

--- hw16/task1.py
+++ hw16/task1.py
@@ -3,7 +3,10 @@
 def chessboard_pattern_new(width, height):
     chessboard = []
     for i in range(height):
-        pattern = [int(j % 2 != i % 2) for j in range(width)] # Перевіряємо на інверсію  та переводимо boolean у int та записуємо у шаблон
+        """
+        Перевіряємо на інверсію  та переводимо boolean у int та записуємо у шаблон
+        """
+        pattern = [int(j % 2 != i % 2) for j in range(width)]
         chessboard.append(pattern)
     return chessboard
 
